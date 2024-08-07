8deebdf312670d043a81cefee8deb8b3fe93721a
MarshandaGra <marshandagracella.15@gmail.com>
2024-08-07 20:04:39 +0700

    data-penyewa-modal

--- resources/views/pesanan/index.blade.php
+++ resources/views/pesanan/index.blade.php
@@ -21,115 +21,25 @@
                 @endif
 
                 <div class="row mt-4">
-                    <div class="col-7">
+                    <div class="col-12">
                         <!-- Header dengan warna biru -->
                         <div class="mb-3">
-                            <h4 class="bg-secondary text-white p-2 rounded">Tambah Data</h4>
+                            <h4 class="bg-secondary text-white p-2 rounded">Data Penyewa</h4>
                         </div>
-                        <form method="post" action="{{ route('pesanan.store') }}"
-                            class="shadow p-3 mb-5 bg-white rounded">
-                            @csrf
-                            <div class="mb-3">
-                                <label for="pemesan" class="form-label">Nama Pemesan</label>
-                                <select name="pemesan" id="pemesan"
-                                    class="form-control  @error('pemesan') is-invalid @enderror">
-                                    <option value="">--Nama Pemesan--</option>
-                                    @foreach ($pemesan as $item)
-                                        <option value="{{ $item->id }}"
-                                            {{ old('pemesan') == $item->id ? 'selected' : '' }}>
-                                            {{ $item->nama_pemesan }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                                @error('pemesan')
-                                    <div class="invalid-feedback">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
+                        
+                        @if ($pesanan->isEmpty())
+                            <div class="alert alert-warning" role="alert">
+                            Data tidak ditemukan.
                             </div>
+                        @else
+                        
+                        <div class="col-7">
                             <div class="mb-3">
-                                <label for="mobil" class="form-label">Mobil</label>
-                                <select name="mobil" id="mobil"
-                                    class="form-control  @error('mobil') is-invalid @enderror">
-                                    <option value="">--Nama mobil--</option>
-                                    @foreach ($mobil as $item)
-                                        <option value="{{ $item->id }}"
-                                            {{ old('mobil') == $item->id ? 'selected' : '' }}>
-                                            {{ $item->nama_m }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                                @error('mobil')
-                                    <div class="invalid-feedback">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
-                            <div class="mb-3">
-                                <label for="bayar" class="form-label">Bayar</label>
-                                <select name="bayar" id="bayar"
-                                    class="form-control  @error('bayar') is-invalid @enderror">
-                                    <option value="">--Jenis Bayar--</option>
-                                    @foreach ($bayar as $item)
-                                        <option value="{{ $item->id }}"
-                                            {{ old('bayar') == $item->id ? 'selected' : '' }}>
-                                            {{ $item->jenis_bayar }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                                @error('bayar')
-                                    <div class="invalid-feedback">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-6">
-                                    <label for="tanggal_mulai" class="form-label">Tanggal Mulai</label>
-                                    <input type="date" name="tanggal_mulai" value="{{ old('tanggal_mulai') }}"
-                                        class="form-control  @error('tanggal_mulai') is-invalid @enderror"
-                                        id="tanggal_mulai">
-                                    @error('tanggal_mulai')
-                                        <div class="invalid-feedback">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
-                                    <div class="row">
-                                        <div class="mb-3">
-                                            <h4 class="bg-secondary text-white p-2 rounded">Data Penyewa</h4>
-                                        </div>
-                                        <form method="GET" action="{{ route('pesanan.index') }}">
-                                            @if (session()->has('success'))
-                                                <div class="alert alert-success mt-1" role="alert">
-                                                    {{ session('success') }}
-                                                </div>
-                                            @endif
-                                            @if (session()->has('danger'))
-                                                <div class="alert alert-danger mt-1" role="alert">
-                                                    {{ session('danger') }}
-                                                </div>
-                                            @endif
-                                            <div class="input-group mb-3">
-                                                <input type="text" name="search"
-                                                    class="form-control mr-2 rounded shadow"
-                                                    placeholder="Cari Penyewa..." value="{{ $search }}">
-                                                <button class="btn btn-outline-secondary rounded shadow"
-                                                    type="submit">Cari</button>
-                                            </div>
-                                        </form>
-                                        @if ($pesanan->isEmpty())
-                                            <div class="alert alert-warning" role="alert">
-                                                Data tidak ditemukan.
-                                            </div>
-                                        @else
-                                            <div class="col-7">
-                                                <div class="mb-3">
-                                                    <button type="button" class="btn btn-primary"
-                                                        data-bs-toggle="modal" data-bs-target="#addDataModal">
-                                                        <i class="fa-solid fa-plus"></i>
-                                                        <span>Tambah Data</span>
-                                                    </button>
-                                                </div>
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addDataModal">
+                                <i class="fa-solid fa-plus"></i>
+                                    <span>Tambah Data</span>
+                                </button>
+                        </div>
 
                                                 <!-- Modal -->
                                                 <div class="modal fade" id="addDataModal" tabindex="-1"
@@ -246,7 +156,7 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                            <table class="table table-bordered ml-3 p-2">
+                                            <table class="table table-bordered">
                                                 <thead>
                                                     <tr>
                                                         <th>No</th>
@@ -274,9 +184,9 @@
                                                             </td>
                                                             <td>
                                                                 <a href="{{ route('pesanan.edit', $data->id) }}"
-                                                                    class="btn btn-warning">Edit</a>
+                                                                    class="btn btn-warning p-2">Edit</a>
                                                                 <a href="{{ route('pesanan.kembali.form', $data->id) }}"
-                                                                    class="btn btn-success"
+                                                                    class="btn btn-success p-2"
                                                                     onclick="return confirm('Apakah mobil sudah kembali?')">Kembalikan</a>
 
                                                                 <form
@@ -284,7 +194,7 @@
                                                                     method="POST" style="display: inline">
                                                                     @csrf
                                                                     @method('DELETE')
-                                                                    <button class="btn btn-danger pr-3"
+                                                                    <button class="btn btn-danger pr-3 p-2"
                                                                         onclick="return confirm('Apakah anda yakin ingin menghapus data ini?')">Hapus</button>
                                                                 </form>
 
